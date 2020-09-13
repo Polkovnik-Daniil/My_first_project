73d5540954f1f95f280abf961a8b99f20ee146c4
Polkovnik-Daniil <0205danik@mail.ru>
2020-09-13 15:52:12 +0300

    commit after fail

--- Git/Class1.cs
+++ Git/Class1.cs
@@ -10,7 +10,7 @@ namespace Git
     {
         static void Main(string[] args)
         {
-            string hello = "Привет мир";
+            string hello = "Привfет мир";
             Console.WriteLine(hello);
             Console.WriteLine("Добро пожаловать в C#!");
             //Console.WriteLine("Пока мир...");
